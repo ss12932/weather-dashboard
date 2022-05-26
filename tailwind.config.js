41bb8f215025b3cc00c3f0ca48cf44a275adca25
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-05-27 00:26:41 +0100

    index.js + moment.js + jquery linked

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./*.html"],
+  content: ["./*.html", "./assets/js/*.js"],
   theme: {
     screens: {
       sm: "480px",
@@ -9,7 +9,18 @@ module.exports = {
       "2xl": "1536px",
     },
     extend: {
-      colors: {},
+      colors: {
+        "rich-black-fogra-29": "#001219ff",
+        "blue-sapphire": " #005f73ff",
+        "viridian-green": "#0a9396ff",
+        "middle-blue-green": "#94d2bdff",
+        "medium-champagne": "#e9d8a6ff",
+        gamboge: "#ee9b00ff",
+        "alloy-orange": "#ca6702ff",
+        rust: "#bb3e03ff",
+        rufous: "#ae2012ff",
+        "ruby-red": "#9b2226ff",
+      },
     },
   },
   plugins: [],
