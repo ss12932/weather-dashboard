a41bc2f211c196ca6a931b8345ec5b7fbd364e86
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-05-26 23:12:58 +0100

    tailwind css setup + folder structure

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    extend: {
+      colors: {},
+    },
+  },
+  plugins: [],
+};
